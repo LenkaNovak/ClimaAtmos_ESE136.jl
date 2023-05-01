818b78e32bf31e6151c2e9c86a8bd065e70c74fa
LenkaNovak <lenka@caltech.edu>
2023-05-01 00:06:26 -0700

    init

---
+++ examples/hybrid/postprocessing.jl
@@ -0,0 +1,130 @@
+import JSON
+using Test
+import OrderedCollections
+using ClimaCoreTempestRemap
+using ClimaCorePlots, Plots
+using ClimaCoreMakie, CairoMakie
+include(joinpath(pkgdir(CA), "post_processing", "contours_and_profiles.jl"))
+include(joinpath(pkgdir(CA), "post_processing", "post_processing_funcs.jl"))
+include(
+    joinpath(pkgdir(CA), "post_processing", "define_tc_quicklook_profiles.jl"),
+)
+
+reference_job_id = parse_arg(parsed_args, "reference_job_id", simulation.job_id)
+
+is_edmfx = atmos.turbconv_model isa CA.EDMFX
+if is_edmfx && parsed_args["post_process"]
+    contours_and_profiles(simulation.output_dir, reference_job_id)
+    zip_and_cleanup_output(simulation.output_dir, "hdf5files.zip")
+end
+
+if parsed_args["debugging_tc"] && !is_edmfx
+    include(
+        joinpath(
+            @__DIR__,
+            "..",
+            "..",
+            "regression_tests",
+            "self_reference_or_path.jl",
+        ),
+    )
+
+    main_branch_root = get_main_branch_buildkite_path()
+    main_branch_data_path = joinpath(main_branch_root, reference_job_id)
+
+    day = floor(Int, simulation.t_end / (60 * 60 * 24))
+    sec = floor(Int, simulation.t_end % (60 * 60 * 24))
+
+    zip_file = "hdf5files.zip"
+    mktempdir(
+        simulation.output_dir;
+        prefix = "temp_unzip_path_",
+    ) do temp_main_branch_path
+        # Unzip files to temp directory, to avoid collisions with other jobs
+        unzip_file_in_path(
+            main_branch_data_path,
+            zip_file,
+            temp_main_branch_path,
+        )
+        # hdf5 files from the main branch are in `temp_dir`
+
+        plot_tc_contours(
+            simulation.output_dir;
+            main_branch_data_path = temp_main_branch_path,
+        )
+        plot_tc_profiles(
+            simulation.output_dir;
+            hdf5_filename = "day$day.$sec.hdf5",
+            main_branch_data_path = temp_main_branch_path,
+        )
+    end
+    if atmos.model_config isa CA.SingleColumnModel
+        zip_and_cleanup_output(simulation.output_dir, zip_file)
+    end
+end
+
+if sol_res.ret_code == :simulation_crashed
+    error(
+        "The ClimaAtmos simulation has crashed. See the stack trace for details.",
+    )
+end
+# Simulation did not crash
+(; sol, walltime) = sol_res
+@assert last(sol.t) == simulation.t_end
+CA.verify_callbacks(sol.t)
+
+if simulation.is_distributed
+    CA.export_scaling_file(
+        sol,
+        simulation.output_dir,
+        walltime,
+        comms_ctx,
+        ClimaComms.nprocs(comms_ctx),
+    )
+end
+
+if !simulation.is_distributed &&
+   parsed_args["post_process"] &&
+   !is_edmfx &&
+   !(atmos.model_config isa CA.SphericalModel)
+    ENV["GKSwstype"] = "nul" # avoid displaying plots
+    if CA.is_column_without_edmf(parsed_args)
+        custom_postprocessing(sol, simulation.output_dir, p)
+    elseif CA.is_column_edmf(parsed_args)
+        postprocessing_edmf(sol, simulation.output_dir, fps)
+    elseif CA.is_solid_body(parsed_args)
+        postprocessing(sol, simulation.output_dir, fps)
+    elseif atmos.model_config isa CA.BoxModel
+        postprocessing_box(sol, simulation.output_dir)
+    elseif atmos.model_config isa CA.PlaneModel
+        postprocessing_plane(sol, simulation.output_dir, p)
+    else
+        error("Uncaught case")
+    end
+end
+
+if parsed_args["regression_test"]
+    # Test results against main branch
+    include(
+        joinpath(
+            @__DIR__,
+            "..",
+            "..",
+            "regression_tests",
+            "regression_tests.jl",
+        ),
+    )
+    perform_regression_tests(
+        simulation.job_id,
+        sol.u[end],
+        all_best_mse,
+        simulation.output_dir,
+    )
+end
+
+if parsed_args["check_conservation"]
+    @test sum(sol.u[1].c.ρ) ≈ sum(sol.u[end].c.ρ) rtol = 25 * eps(FT)
+    @test sum(sol.u[1].c.ρe_tot) +
+          (p.net_energy_flux_sfc[][] - p.net_energy_flux_toa[][]) ≈
+          sum(sol.u[end].c.ρe_tot) rtol = 30 * eps(FT)
+end
